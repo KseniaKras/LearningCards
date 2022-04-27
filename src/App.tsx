37bc43cbf285823ba9d5bdc8e0b7bc4fe5212bef
Ksenia <ksenia.kras29@gmail.com>
2022-04-27 18:31:06 +0300

    refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
-import {NavLink} from 'react-router-dom';
+import {HashRouter, NavLink} from 'react-router-dom';
 import {Routes, Route} from 'react-router-dom';
 import './App.css';
 import {SuperComponents} from "./components/SuperComponents";
 import {LoginPage} from "./components/LoginPage";
 import {ErrorPage} from "./components/ErrorPage";
 import {ProfilePage} from "./components/ProfilePage";
+import {RegistryPage} from "./components/RegistryPage";
 
 export const PATH = {
     LOGIN: '/login',
     ERROR: '/error',
     TEST: '/test',
     PROFILE: '/profile',
+    REGISTRY: '/registry',
     // add paths
 }
 
@@ -21,20 +23,22 @@ function App() {
             <div>
                 <Header/>
             </div>
+            <HashRouter>
+                <Routes>
+                    <Route path={'/'} element={<SuperComponents/>}/>
+                    <Route path={PATH.LOGIN} element={<LoginPage/>}/>
+                    <Route path={PATH.ERROR} element={<ErrorPage/>}/>
+                    <Route path={PATH.PROFILE} element={<ProfilePage/>}/>
+                    <Route path={PATH.REGISTRY} element={<RegistryPage/>}/>
 
-            <Routes>
-                <Route path={'/'} element={<SuperComponents/>}/>
-                <Route path={PATH.LOGIN} element={<LoginPage/>}/>
-                <Route path={PATH.ERROR} element={<ErrorPage/>}/>
-                <Route path={PATH.PROFILE} element={<ProfilePage/>}/>
-
-                {/*<Route path={"/registration"} element={<h3>регистрация</h3>}/>*/}
-                {/*<Route path={"/profile"} element={<h3>профайл</h3>}/>*/}
-                {/*<Route path={"/error"} element={<h3>404</h3>}/>*/}
-                {/*<Route path={"/verifypassword"} element={<h3>восстановление пароля</h3>}/>*/}
-                {/*<Route path={"/createpassword"} element={<h3>ввод нового пароля</h3>}/>*/}
-                {/*<Route path="/test" element={<SuperComponents/>}/>*/}
-            </Routes>
+                    {/*<Route path={"/registration"} element={<h3>регистрация</h3>}/>*/}
+                    {/*<Route path={"/profile"} element={<h3>профайл</h3>}/>*/}
+                    {/*<Route path={"/error"} element={<h3>404</h3>}/>*/}
+                    {/*<Route path={"/verifypassword"} element={<h3>восстановление пароля</h3>}/>*/}
+                    {/*<Route path={"/createpassword"} element={<h3>ввод нового пароля</h3>}/>*/}
+                    {/*<Route path="/test" element={<SuperComponents/>}/>*/}
+                </Routes>
+            </HashRouter>
         </div>
     );
 }
@@ -46,13 +50,13 @@ export const Header = () => {
 
     return (
         <div style={{display: "flex", justifyContent: "space-around", width: "60%", margin: "0 auto"}}>
-            <NavLink to={"login"}> login </NavLink>
-            {/*<NavLink to={"registration"}> registration </NavLink>*/}
-            <NavLink to={"profile"}> profile </NavLink>
-            <NavLink to={"error"}> error </NavLink>
+            <NavLink to={PATH.LOGIN}> login </NavLink>
+            <NavLink to={PATH.REGISTRY}> registration </NavLink>
+            <NavLink to={PATH.PROFILE}> profile </NavLink>
+            <NavLink to={PATH.ERROR}> error </NavLink>
             {/*<NavLink to={"verifypassword"}> verify password </NavLink>*/}
             {/*<NavLink to={"createpassword"}> new password </NavLink>*/}
-            <NavLink to={"test"}> test </NavLink>
+            <NavLink to={PATH.TEST}> test </NavLink>
         </div>
     )
 }
---
+++ src/components/RegistryPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const RegistryPage = () => {
+    return (
+        <div>
+            Registration
+        </div>
+    );
+};
