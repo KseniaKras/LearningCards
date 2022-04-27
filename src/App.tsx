c134f6511961a423ca3762ad8b808550278a9db0
Ksenia <ksenia.kras29@gmail.com>
2022-04-27 18:08:20 +0300

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,27 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
+import {Routes, Route, Navigate} from 'react-router-dom';
 import './App.css';
 import {SuperComponents} from "./components/SuperComponents";
 
 function App() {
-  return (
-    <div className="App">
-        <div>
-            <Header />
+    return (
+        <div className="App">
+            <div>
+                <Header/>
+            </div>
+            <Routes>
+                <Route path={"/"} element={<SuperComponents/>}/>
+                <Route path={"/login"} element={<h3>логинизация</h3>}/>
+                <Route path={"/registration"} element={<h3>регистрация</h3>}/>
+                <Route path={"/profile"} element={<h3>профайл</h3>}/>
+                <Route path={"/error"} element={<h3>404</h3>}/>
+                <Route path={"/verifypassword"} element={<h3>восстановление пароля</h3>}/>
+                <Route path={"/createpassword"} element={<h3>ввод нового пароля</h3>}/>
+                {/*<Route path="/test" element={<SuperComponents/>}/>*/}
+            </Routes>
         </div>
-      <Routes>
-          <Route path="/" element={<SuperComponents/>}/>
-          <Route path="login" element={"логинизация"}/>
-          <Route path="registration" element={"регистрация"}/>
-          <Route path="profile" element={"профайл"}/>
-          <Route path="error" element={"404"}/>
-          <Route path="verifypassword" element={"восстановление пароля"}/>
-          <Route path="createpassword" element={"ввод нового пароля"}/>
-          <Route path="test" element={<SuperComponents/>}/>
-      </Routes>
-    </div>
-  );
+    );
 }
 
 export default App;
@@ -31,13 +31,13 @@ export const Header = () => {
 
     return (
         <div style={{display: "flex", justifyContent: "space-around", width: "60%", margin: "0 auto"}}>
-            <NavLink to={'login'}> login </NavLink>
-            <NavLink to={'registration'} > registration </NavLink>
-            <NavLink to={'profile'} > profile </NavLink>
-            <NavLink to={'error'} > error </NavLink>
-            <NavLink to={'verifypassword'} > verify password </NavLink>
-            <NavLink to={'createpassword'} > new password </NavLink>
-            <NavLink to={'test'} > test </NavLink>
+            <NavLink to={"login"}> login </NavLink>
+            <NavLink to={"registration"}> registration </NavLink>
+            <NavLink to={"profile"}> profile </NavLink>
+            <NavLink to={"error"}> error </NavLink>
+            <NavLink to={"verifypassword"}> verify password </NavLink>
+            <NavLink to={"createpassword"}> new password </NavLink>
+            <NavLink to={"test"}> test </NavLink>
         </div>
     )
 }
